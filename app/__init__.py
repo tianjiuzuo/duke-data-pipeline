c5d867aac467e89b028bdacd18b4fb3caded7c28
Tianjiu Zuo <tianjiu.zuo@gmail.com>
2020-08-10 04:32:14 -0400

    fixing static folder path finding

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 from config import Config
 from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='app/')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
