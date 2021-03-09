80e45ef47744b93dddb9ce3989020b3933b80035
Qi Xuan Khoo <khooqixuan@moneymatch.co>
2021-03-08 21:39:34 -0500

    set up database model relationships

--- app/models.py
+++ app/models.py
@@ -9,7 +9,10 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    organization = db.Column(db.String(120))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    updates = db.relationship('Update', backref='shelter', lazy=True)
+    requests = db.relationship('Request', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -35,11 +38,24 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
-class CollectionForm(db.Model):
-    organization = db.Column(db.String(200), primary_key=True)
+
+class Update(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     number_of_victims = db.Column(db.Integer)
     capacity = db.Column(db.Integer)
-    user_id2 = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<CollectionForm {}>'.format(self.organization)
+        return '<Update {}>'.format(self.user_id)
+
+
+class Request(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    data_requested = db.Column(
+        db.String(), nullable=False)  #csv strings of requested data fields
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Request {}>'.format(self.user_id)
