b5ee2d7c61b4f7a5cde910b1fd5c92f682dfb98a
yvonne kuo <yvonne.kuo@duke.edu>
2021-03-10 01:33:22 -0500

    added change password feature, mandated password length

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,9 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, CollectionForm
+from app.forms import LoginForm, RegistrationForm, CollectionForm, ChangePasswordForm
 from app.models import User
-
+from werkzeug.security import generate_password_hash
 
 @app.route('/')
 @app.route('/index')
@@ -46,6 +46,18 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
+@app.route('/changePassword', methods=['GET', 'POST'])
+@login_required
+def changePassword():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=current_user.username).first()
+        user.password_hash = generate_password_hash(form.password.data)
+        db.session.commit()
+        flash('Password Updated!')
+        return redirect(url_for('index'))
+    return render_template('changePassword.html', title='Change Password', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
